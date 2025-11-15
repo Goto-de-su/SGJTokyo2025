46e4929743c814b1cc6f828c3d7222f1cc43bf91
Martin_Chen <chynokagami@gmail.com>
2025-11-15 22:02:29 +0900

    uifinishmenu
    uifinishedmenu

--- Assets/Script/SceneButton.cs
+++ Assets/Script/SceneButton.cs
@@ -1,10 +1,12 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class SceneButton : MonoBehaviour
 {
     [SerializeField] private string sceneName = "Main";
+    [SerializeField] private AudioSource clickSound; // 拖这里
 
     void Start()
     {
@@ -13,6 +15,16 @@ public class SceneButton : MonoBehaviour
 
     public void LoadSceneOnClick()
     {
+        StartCoroutine(PlaySoundAndLoad());
+    }
+
+    private IEnumerator PlaySoundAndLoad()
+    {
+        if (clickSound != null)
+        {
+            clickSound.Play();
+            yield return new WaitForSeconds(clickSound.clip.length);
+        }
         SceneManager.LoadScene(sceneName);
     }
 }
