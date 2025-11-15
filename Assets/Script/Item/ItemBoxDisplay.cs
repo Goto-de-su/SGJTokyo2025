ebe79993d37337c1791e780670e206063757ae73
Col <tasakana1998@gmail.com>
2025-11-15 12:14:41 +0900

    ItemのViewクラスを修正

--- Assets/Script/Item/ItemBoxDisplay.cs
+++ Assets/Script/Item/ItemBoxDisplay.cs
@@ -1,15 +1,16 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemBoxDisplay : MonoBehaviour
 {
     [Tooltip("アイテムボックスのスプライトレンダラー")]
-    [SerializeField] private SpriteRenderer itemSpriteRenderer;
+    [SerializeField] private Image image;
 
     /// <summary>
     /// アイテムボックスにアイテム表示
     /// </summary>
     public void DisplayItem(ItemData inputData)
     {
-        this.itemSpriteRenderer.sprite = inputData.icon;
+        this.image.sprite = inputData.icon;
     }
 }
